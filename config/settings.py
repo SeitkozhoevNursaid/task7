a7b7087d7edf491e12c5bf567782f1e94487be7e
Сейткожоев Нурсаид Рахатбекович <NSeytkozhoev@skymobile.local>
2024-06-05 14:40:40 +0600

    ggggg

--- config/settings.py
+++ config/settings.py
@@ -15,8 +15,10 @@ from datetime import timedelta
 from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# adadadada
-#2adadadadadadadad
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+for i in range(1, 10):
+    break
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
